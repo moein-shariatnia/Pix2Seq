08e0397c9fe062f7f981544faaa2297098f79d33
moein <moein.shariatnia@gmail.com>
2022-08-17 22:02:41 +0430

    added test.py

--- model.py
+++ model.py
@@ -24,62 +24,85 @@ class Decoder(nn.Module):
     def __init__(self, vocab_size, encoder_length, dim, num_heads, num_layers):
         super().__init__()
         self.dim = dim
-
+        
         self.embedding = nn.Embedding(vocab_size, dim)
-        self.decoder_pos_embed = nn.Parameter(
-            torch.randn(1, CFG.max_len-1, dim) * .02)
+        self.decoder_pos_embed = nn.Parameter(torch.randn(1, CFG.max_len-1, dim) * .02)
         self.decoder_pos_drop = nn.Dropout(p=0.05)
-
-        decoder_layer = nn.TransformerDecoderLayer(
-            d_model=dim, nhead=num_heads)
-        self.decoder = nn.TransformerDecoder(
-            decoder_layer, num_layers=num_layers)
+        
+        decoder_layer = nn.TransformerDecoderLayer(d_model=dim, nhead=num_heads)
+        self.decoder = nn.TransformerDecoder(decoder_layer, num_layers=num_layers)
         self.output = nn.Linear(dim, vocab_size)
-
-        self.encoder_pos_embed = nn.Parameter(
-            torch.randn(1, encoder_length, dim) * .02)
+        
+        
+        self.encoder_pos_embed = nn.Parameter(torch.randn(1, encoder_length, dim) * .02)
         self.encoder_pos_drop = nn.Dropout(p=0.05)
-
+        
         self.init_weights()
-
+        
     def init_weights(self):
         for name, p in self.named_parameters():
-            if 'encoder_pos_embed' in name or 'decoder_pos_embed' in name:
+            if 'encoder_pos_embed' in name or 'decoder_pos_embed' in name: 
                 print("skipping pos_embed...")
                 continue
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
-
+                
         trunc_normal_(self.encoder_pos_embed, std=.02)
         trunc_normal_(self.decoder_pos_embed, std=.02)
-
+        
+    
     def forward(self, encoder_out, tgt):
         """
         encoder_out: shape(N, L, D)
         tgt: shape(N, L)
         """
-
+        
         tgt_mask, tgt_padding_mask = create_mask(tgt)
-
         tgt_embedding = self.embedding(tgt)
         tgt_embedding = self.decoder_pos_drop(
             tgt_embedding + self.decoder_pos_embed
         )
-
+        
         encoder_out = self.encoder_pos_drop(
             encoder_out + self.encoder_pos_embed
         )
-
+        
         encoder_out = encoder_out.transpose(0, 1)
         tgt_embedding = tgt_embedding.transpose(0, 1)
-
-        preds = self.decoder(memory=encoder_out,
+        
+        preds = self.decoder(memory=encoder_out, 
                              tgt=tgt_embedding,
-                             tgt_mask=tgt_mask,
+                             tgt_mask=tgt_mask, 
                              tgt_key_padding_mask=tgt_padding_mask)
-
+        
         preds = preds.transpose(0, 1)
         return self.output(preds)
+    
+    def predict(self, encoder_out, tgt):
+        length = tgt.size(1)
+        padding = torch.ones(tgt.size(0), CFG.max_len-length-1).fill_(CFG.pad_idx).long().to(tgt.device)
+        tgt = torch.cat([tgt, padding], dim=1)
+        tgt_mask, tgt_padding_mask = create_mask(tgt)
+        # is it necessary to multiply it by math.sqrt(d) ?
+        tgt_embedding = self.embedding(tgt)
+        tgt_embedding = self.decoder_pos_drop(
+            tgt_embedding + self.decoder_pos_embed
+        )
+        
+        encoder_out = self.encoder_pos_drop(
+            encoder_out + self.encoder_pos_embed
+        )
+        
+        encoder_out = encoder_out.transpose(0, 1)
+        tgt_embedding = tgt_embedding.transpose(0, 1)
+        
+        preds = self.decoder(memory=encoder_out, 
+                             tgt=tgt_embedding,
+                             tgt_mask=tgt_mask, 
+                             tgt_key_padding_mask=tgt_padding_mask)
+        
+        preds = preds.transpose(0, 1)
+        return self.output(preds)[:, length-1, :]
 
 class EncoderDecoder(nn.Module):
     def __init__(self, encoder, decoder):
@@ -90,4 +113,8 @@ class EncoderDecoder(nn.Module):
     def forward(self, image, tgt):
         encoder_out = self.encoder(image)
         preds = self.decoder(encoder_out, tgt)
+        return preds
+    def predict(self, image, tgt):
+        encoder_out = self.encoder(image)
+        preds = self.decoder.predict(encoder_out, tgt)
         return preds
---
+++ test.py
@@ -0,0 +1,103 @@
+
+import torch
+from torch import nn
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+from transformers import top_k_top_p_filtering
+
+from utils import seed_everything
+from preprocess import build_df
+from dataset import split_df, get_loaders
+from tokenizer import Tokenizer
+from model import Encoder, Decoder, EncoderDecoder
+from config import CFG
+
+def generate_batch(model, x, max_len=50, top_k=0, top_p=1):
+    x = x.to(CFG.device)
+    batch_preds = torch.ones(x.size(0), 1).fill_(tokenizer.BOS_code).long().to(CFG.device)
+    confs = []
+    
+    if top_k != 0 or top_p != 1:
+        sample = lambda preds: torch.softmax(preds, dim=-1).multinomial(num_samples=1).view(-1, 1)
+    else:
+        sample = lambda preds: torch.softmax(preds, dim=-1).argmax(dim=-1).view(-1, 1)
+        
+    with torch.no_grad():
+        for i in range(max_len):
+#             preds = model.predict(x, batch_preds)[:, -1, :]
+            preds = model.predict(x, batch_preds)
+            ## If top_k and top_p are set to default, the following line does nothing!
+            preds = top_k_top_p_filtering(preds, top_k=top_k, top_p=top_p)
+            if i % 4 == 0:
+                confs_ = torch.softmax(preds, dim=-1).sort(axis=-1, descending=True)[0][:, 0].cpu()
+                confs.append(confs_)
+            preds = sample(preds)
+            batch_preds = torch.cat([batch_preds, preds], dim=1)
+    
+    return batch_preds.cpu(), confs
+
+def extract_bboxes(batch_preds, confs, EOS_code):
+    EOS_idxs = (batch_preds == EOS_code).float().argmax(dim=-1)
+    ## sanity check
+    invalid_idxs = ((EOS_idxs - 1) % 5 != 0).nonzero().view(-1)
+    EOS_idxs[invalid_idxs] = 0
+    
+    bboxes = []
+    for i, EOS_idx in enumerate(EOS_idxs.tolist()):
+        if EOS_idx == 0:
+            bboxes.append(None)
+            continue
+        bbox = batch_preds[i, 1:EOS_idx].tolist()
+        bbox = [bbox[i: i+5] for i in range(0, len(bbox), 5)]
+        ## adding classification confidence for each pred
+        for j in range(len(bbox)):
+            confidence = confs[j][i]
+            bbox[j].append(round(confidence.item(), 3))
+        
+        bboxes.append(bbox)
+    return bboxes
+
+
+if __name__ == '__main__':
+    seed_everything(42)
+
+    IMG_FILES = glob(CFG.img_path + "/*.jpg")
+    XML_FILES = glob(CFG.xml_path + "/*.xml")
+    assert len(IMG_FILES) == len(
+        XML_FILES) != 0, "images or xml files not found"
+    print("Number of found images: ", len(IMG_FILES))
+
+    df, classes = build_df(XML_FILES)
+    # build id to class name and vice verca mappings
+    cls2id = {cls_name: i for i, cls_name in enumerate(classes)}
+    id2cls = {i: cls_name for i, cls_name in enumerate(classes)}
+
+    train_df, valid_df = split_df(df)
+    print("Train size: ", train_df['id'].nunique())
+    print("Valid size: ", valid_df['id'].nunique())
+
+    tokenizer = Tokenizer(num_classes=len(classes), num_bins=CFG.num_bins,
+                          width=CFG.img_size, height=CFG.img_size, max_len=CFG.max_len)
+    CFG.pad_idx = tokenizer.PAD_code
+    train_loader, valid_loader = get_loaders(
+        train_df, valid_df, tokenizer, CFG.img_size, CFG.batch_size, CFG.max_len, tokenizer.PAD_code)
+
+    encoder = Encoder(model_name=CFG.model_name, pretrained=False, out_dim=256)
+    decoder = Decoder(vocab_size=tokenizer.vocab_size,
+                      encoder_length=CFG.num_patches, dim=256, num_heads=8, num_layers=6)
+    model = EncoderDecoder(encoder, decoder)
+    model.to(CFG.device)
+    
+    msg = model.load_state_dict(torch.load('./pix2seq_weights.pth', map_location=CFG.device))
+    print(msg)
+    model.eval()
+    
+    all_bboxes = []
+    with torch.no_grad():
+        for x, _ in tqdm(valid_loader):
+            batch_preds, confs = generate_batch(model, x, max_len=21, top_k=0, top_p=1)
+            bboxes = extract_bboxes(batch_preds, confs, tokenizer.EOS_code)
+            all_bboxes.extend(bboxes)
+    
+
